8c828250946fc93f05c7cdb49de45b4a1d967185
Luke <saop0071288617@gmail.com>
2023-05-16 22:46:58 +0800

    fix typo of app name

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'review_djanog.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'review_django.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- review_django/urls.py
+++ review_django/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for review_djanog project.
+URL configuration for review_django project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
