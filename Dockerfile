3ebbe2e815eb78016d41d44863d9fd4a2d681378
Luke <saop0071288617@gmail.com>
2023-05-16 18:43:55 +0800

    Django Postgres in Docker for deploy

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.11
+
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+WORKDIR /app
+COPY . .
+RUN pip install -r requirements.txt
+
+
+CMD python manage.py runserver 0.0.0.0:8000
--- review_djanog/settings.py
+++ review_djanog/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'review_djanog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_database_name',
+        'USER': 'your_username',
+        'PASSWORD': 'your_password',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
